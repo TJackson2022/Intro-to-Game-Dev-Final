ac7e209a9b996d8f470f1d1943ee5771f3f53f0d
Tom Jackson <tjackson5@ltu.edu>
2022-11-16 17:24:17 -0500

    Update to PlayerControl, some pseudocode

--- Assets/Scripts/enemyControl.cs
+++ Assets/Scripts/enemyControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyControl : MonoBehaviour
+public class EnemyControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerControl : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Reference to the character controller that handles our collisions and movement
+    public CharacterController controller;
+
+    // Speed of the player
+    public float speed = 1f;
 
     // Update is called once per frame
     void Update()
+    {
+        Vector2 currentPos = transform.position;
+        Vector2 motion = Vector2.zero;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        motion = (transform.forward * speed * inputY * Time.deltaTime) + (transform.right * speed * inputX * Time.deltaTime);
+
+        controller.Move(motion);
+    }
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
