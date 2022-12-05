4da2684cd125d9ffe2508edc93a248c62093335b
Tom Jackson <tjackson5@ltu.edu>
2022-12-05 13:36:25 -0500

    Fixed EnemyControl - Enemies will again chase player
    Still spinning weirdly though

--- Assets/Scripts/enemyControl.cs
+++ Assets/Scripts/enemyControl.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public float moveSpeed;
     public Rigidbody rb;
-    private Vector2 movement;
+    private Vector3 movement;
     public GameObject Enemy;
 
     public float gravMod = 1.0f;
 
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
+        Vector3 direction = player.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x);// * Mathf.Rad2Deg;
         rb.transform.Rotate(0, 0, angle);
         direction.Normalize();
@@ -23,16 +28,14 @@ public class EnemyControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //if (FindObjectOfType<BatActivate>().activateBats == true)
-        //{
         moveCharacter(movement);
-        //}
     }
 
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.deltaTime);
     }
+
     //public void OnTriggerEnter2D(Collider2D collision)
     //{
     //    if (collision.gameObject.tag == "Shot")
